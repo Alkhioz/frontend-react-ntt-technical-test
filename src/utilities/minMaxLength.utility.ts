11f81be2442e4ace1729f5a26ed8eb66fabf06c3
Alejandro Mendoza <33111241+Alkhioz@users.noreply.github.com>
2024-07-13 13:38:34 -0500

    Added addtodate utility

--- src/utilities/minMaxLength.utility.ts
+++ src/utilities/minMaxLength.utility.ts
@@ -1,5 +1,11 @@
-export function minMaxLength(str: string, min: number = 0, max?: number) {
+/**
+ * Checks if a string is between a min and max value.
+ * @param {string} str - The string to evaluate.
+ * @param {number} min - The minimun number of characters.
+ * @param {number} max - The maximun number of characters
+ * @returns {boolean} 
+ */
+export function minMaxLength(str: string, min: number, max: number) {
     const strlen = str.length;
-    if(!max) return strlen >= min;
     return strlen >= min && strlen <= max;
 }
