4e10217fc84cb582691db112829874ed3d3adf78
Alejandro Mendoza <33111241+Alkhioz@users.noreply.github.com>
2024-07-14 18:59:19 -0500

    Fixed cors error on backend

--- repo-interview-main/src/main.ts
+++ repo-interview-main/src/main.ts
@@ -1,19 +1,39 @@
-import { createExpressServer } from "routing-controllers";
+import { createExpressServer, useExpressServer } from "routing-controllers";
 import 'dotenv/config';
+import express from 'express';
+import { ProductController } from "./controllers/ProductControllers";
 
 let PORT = 3002;
 
-// creates express app, registers all controller routes and returns you express app instance
-const app = createExpressServer({
-  //cors: true,
-  routePrefix: "/bp", 
+// CORS middleware
+const corsMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // End the response for OPTIONS requests
+  } else {
+    next(); // Proceed to the next middleware for other requests
+  }
+};
 
+// creates express app
+const app = express();
+
+// Use CORS middleware before routing-controllers
+app.use(corsMiddleware);
+
+// Register routes with routing-controllers
+useExpressServer(app, {
+  routePrefix: "/bp",
   controllers: [
-    __dirname + "/controllers/*{.js,.ts}",
-  ], // we specify controllers we want to use
+    ProductController,
+  ], // specify controllers
 });
 
-// run express application on port 3000
+// run express application on specified port
 app.listen(PORT, () => {
   console.log(`Servidor Iniciado`);
   console.log(`Host: http://localhost:${PORT}`);
